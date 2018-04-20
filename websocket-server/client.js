0691dc53cf9aa1005828ed9c33df829d541376e8
EssamTarik <essamtarik2000@gmail.com>
2018-04-20 18:41:48 +0200

    websocket real backend response

--- websocket-server/client.js
+++ websocket-server/client.js
@@ -1,17 +1,17 @@
 var io = require('socket.io-client');
 var socket = io('http://localhost:3000');
 
-setInterval(function(){
-socket.emit('update', {path: 'testdb.testcollection', data: 'hello world'});
-}, 2000);
+// setInterval(function(){
+// socket.emit('update', {path: 'testdb.testcollection', data: 'hello world'});
+// }, 2000);
 
 function listenToPath(path, callback){
-	socket.emit('listen', {path: 'testdb.testcollection'});
+	socket.emit('listen', {path: path});
 
 	socket.on(path, callback)	
 }
 
-listenToPath('testdb.testcollection', function(data){
+listenToPath('sessiondb.comedyMovies', function(data){
 	console.log('path fetched');
-	console.log(data);
+	console.log(data.message[0].name);
 })
--- websocket-server/index.js
+++ websocket-server/index.js
@@ -1,15 +1,10 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
-});
+var axios = require('axios');
 
 let sockets = {};
 let listenPaths = {};
-
-let db = {'testdb.testcollection': 'first test data', 'testdb2.testcollection2': 'second test data'};
 
 io.on('connection', function(socket){
 	sockets[socket.id] = socket;
@@ -22,25 +17,30 @@ io.on('connection', function(socket){
 			listenPaths[data.path].push(socket.id);
 		else
 			listenPaths[data.path] = [socket.id];
+		emitToPath(data.path, {});
 	})
+
 	socket.on('update', function(data){
-		console.log(Object.keys(sockets));
-		let ids = listenPaths[data.path];
+		emitToPath(data.path)
+	})
+});
+
+function emitToPath(path, condition){
+	let pathArray = path.split('.');
+	let dbname = pathArray[0];
+	let collection = pathArray[1];
+
+	axios.get('http://localhost:5000/findCondition', {params: {dbname, collection, condition}, headers: {AUTHTOKEN: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzd29yZCI6IjEyMyIsImVtYWlsIjoiZXNzYW0iLCJfaWQiOnsiJG9pZCI6IjVhY2JiNmJhN2Y3OGIxMGZmMmRjODEyNiJ9fQ.zhcvC1RjxZcYMPcss2c-by1Fn4u-xpU5nFVNPTE-W8I'}}).then(function(response){
+		console.log(response.data);
+		let ids = listenPaths[path];
 		ids.map(function(id){
 			let client = sockets[id];
 			//update the database here
 			if(client)
-				client.emit(data.path, db[data.path]);
-
+				client.emit(path, response.data);
 		});
 	})
-});
-
-// setTimeout(function(){
-// 	let id = listenPaths['testdb.testcollection'];
-// 	let client = sockets[id];
-// 	client.emit('message', 'for your eyes only');	
-// }, 2000);
+}
 
 http.listen(3000, function(){
   console.log('listening on localhost:3000');
